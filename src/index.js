675ec95c147ac29ba9fe1620fd21e25b1ab035d4
Zheleznikov <polwen@yandex.ru>
2020-05-04 20:04:14 +0300

    13 переключение попапов

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style/index.css';
 import Popup from './js/components/Popup';
 
 (function() {
-  const popup = new Popup(document.querySelector('.popup'));
-  popup.setContent();
+  new Popup(document.querySelector('.popup'));
+
 
 }());
--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -1,20 +1,19 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
 export default class Popup {
   constructor(element) {
     this.element = element;
+    this.toggleButton = this.element.querySelector('.popup__toggle-link');
     this.handlers();
-
-    this.popupContentEnter = document.querySelector('.popup__content-enter').content.cloneNode(true);
-    this.popupContentReg = document.querySelector('.popup__content-reg').content.cloneNode(true);
   }
 
   open() {
+    this.setRegPopup();
     this.element.classList.add('popup_is-opened');
+
   }
 
   close() {
     this.element.classList.remove('popup_is-opened');
+    this.clearContent();
   }
 
   closeByKey(event) {
@@ -29,26 +28,43 @@ export default class Popup {
     }
   }
 
-  setContent() {
-    this.element.querySelector('.popup__content').appendChild(this.popupContentEnter);
-     this.element.querySelector('.popup__toggle-link').addEventListener('click', this.setContent2.bind(this));
-  }
-
-  setContent2() {
-    this.clearContent();
-     this.element.querySelector('.popup__content').appendChild(this.popupContentReg);
-  }
-
-
   clearContent() {
-    this.element.querySelector('.popup__content').innerHTML = '';
+    this.element.querySelector('.popup__template').innerHTML = '';
   }
+
+  setEnterPopup() {
+    this.clearContent();
+    const popupContentEnter = document.querySelector('.popup__content-enter').content.cloneNode(true);
+    this.element.querySelector('.popup__template').appendChild(popupContentEnter);
+    this.toggleButton.textContent = 'Зарегистрироваться';
+    this.toggleButton.classList.add('is-enter');
+    this.toggleButton.classList.remove('is-reg', true);
+  }
+
+  setRegPopup() {
+    this.clearContent();
+    const popupContentReg = document.querySelector('.popup__content-reg').content.cloneNode(true);
+    this.element.querySelector('.popup__template').appendChild(popupContentReg);
+    this.toggleButton.textContent = 'Войти';
+    this.toggleButton.classList.add('is-reg');
+    this.toggleButton.classList.remove('is-enter', true);
+  }
+
+  contentHandler() {
+    if (this.toggleButton.classList.contains('is-enter')) {
+      this.setRegPopup();
+    } else if (this.toggleButton.classList.contains('is-reg')) {
+      this.setEnterPopup();
+    }
+
+  }
+
 
   handlers() {
     document.querySelector('.header__button').addEventListener('click', this.open.bind(this));
     window.addEventListener('keydown', this.closeByKey.bind(this));
     this.element.addEventListener('click', this.closeByClick.bind(this));
+    this.toggleButton.addEventListener('click', this.contentHandler.bind(this));
 
-    //  this.element.querySelector('.popup__toggle-link').addEventListener('click', this.setContent2.bind(this));
   }
 }
